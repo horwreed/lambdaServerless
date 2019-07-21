8f845c8b0ce7f617eb55f7c5c35b8b6d1af3e65c
horwreed <horwreed@umich.edu>
2019-07-21 12:56:57 -0500

    Core functionality done

---
+++ index.js
@@ -0,0 +1,202 @@
+const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const AWS = require('aws-sdk');
+
+
+const PET_TABLE = "pet_table";
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+app.use(bodyParser.json({ strict: false }));
+
+age_factors = {
+'<1' : 1.01 ,
+'1' : 1.015 ,
+'2' : 1.019 ,
+'3' : 1.024 ,
+'4' : 1.028 ,
+'5' : 1.03 ,
+'6' : 1.034 ,
+'7' : 1.038 ,
+'8' : 1.044 ,
+'>8' : 1.055
+}
+
+zipcode_factors = {
+'90210' : 1.01 ,
+'10001' : 1.015 ,
+'02481' : 1.019 ,
+'11217' : 1.024 ,
+'DEFAULT' : 1.013
+}
+
+dog_breed_factors = {
+'golden_retriever' : 1.01 ,
+'dachshund' : 1.015 ,
+'chesapeake_bay_retriever' : 1.002 ,
+'DEFAULT' : 1.005
+}
+
+cat_breed_factors = {
+'siamese' : 1.01 ,
+'maine_coon' : 1.015 ,
+'ragdoll' : 1.002 ,
+'DEFAULT' : 1.005
+}
+
+species_factors = {
+'dog' : 1 ,
+'cat' : 0.99
+}
+
+const BASE_PRICE = 45
+
+function getFactorOrDefault(input, factors) {
+    if (input in factors) {
+        return factors[input];
+    } else {
+        return factors['DEFAULT'];
+    }
+}
+
+function calculateQuote(age, zipcode, breed, species) {
+    var age_factor;
+    var zipcode_factor;
+    var breed_factor;
+    var species_factor;
+
+    if (Number(age) < 1) {
+        age = '<1';
+    }
+    if (Number(age) > 8) {
+        age = '>8';
+    }
+    age_factor = age_factors[age];
+    zipcode_factor = getFactorOrDefault(zipcode, zipcode_factors);
+
+    if (species == 'dog') {
+        species_factor = species_factors[species];
+        breed_factor = getFactorOrDefault(breed, dog_breed_factors);
+    } else if (species == 'cat') {
+        species_factor = species_factors[species];
+        breed_factor = getFactorOrDefault(breed, cat_breed_factors);
+    } else {
+        throw "Cannot calculate quote for species other than dogs and cats";
+    }
+
+    return BASE_PRICE * (age_factor + zipcode_factor + breed_factor + species_factor);
+}
+
+// Get Quote endpoint
+app.get('/quote/:petId', function (req, res) {
+  const params = {
+    TableName: PET_TABLE,
+    Key: {
+      petId: req.params.petId,
+    },
+  }
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not get quote' });
+    }
+    if (result.Item) {
+      const { age, zipcode, breed, species } = result.Item;
+      try {
+        var quote = calculateQuote(age, zipcode, breed, species);
+        res.json({ quote });
+      }
+      catch(err) {
+        res.status(400).json({ error: 'Could not calculate quote: ' + err});
+      }
+    } else {
+      res.status(404).json({ error: "Pet not found" });
+    }
+  });
+})
+
+//Get Pet endpoing
+app.get('/pet/:petId', function(req, res)) {
+    const params = {
+    TableName: PET_TABLE,
+    Key: {
+      petId: req.params.petId,
+    },
+  }
+
+  dynamoDb.get(params, (error, result) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not get quote' });
+    }
+    if (result.Item) {
+      const { petId, age, breed, zipcode, name, gender, species } = result.Item;
+      try {
+        var quote = calculateQuote(age, zipcode, breed, species);
+        res.json({ petId, age, breed, zipcode, name, gender, species });
+      }
+      catch(err) {
+        res.status(400).json({ error: 'Could not calculate quote: ' + err});
+      }
+    } else {
+      res.status(404).json({ error: "Pet not found" });
+    }
+  });
+})
+
+// 1) Validate age is a non negative integer
+// 2) Validate zipcode is a non negative integer
+// 3) Validate species is a dog or a cat, our api only suppports dogs and cats
+function validateInput(petId, age, breed, zipcode, name, gender, species) {
+    var age_n = Number(age);
+    if (Number.isInteger(age_n) == false || age_n < 0) {
+        throw "Age must be a non negative integer";
+    }
+
+    var zipcode_n = Number(zipcode);
+    if (Number.isInteger(zipcode_n) == false || zipcode_n < 0) {
+        throw "Zipcode must ba non negative integer";
+    }
+
+    if (species != "dog" && species != "cat") {
+        throw "The only supported species are dog and cat";
+    }
+}
+
+// Create Pet endpoint
+app.post('/pet', function (req, res) {
+  const { petId, age, breed, zipcode, name, gender, species } = req.body;
+
+  //Validate input before putting it in the db
+  try {
+    validateInput(petId, age, breed, zipcode, name, gender, species);
+  }
+  catch(err) {
+    res.status(400).json({ error: 'Could not create pet: ' + err});
+  }
+
+  const params = {
+    TableName: PET_TABLE,
+    Item: {
+      petId: petId,
+      age: age,
+      breed: breed,
+      zipcode: zipcode,
+      name: name,
+      gender: gender,
+      species: species
+    },
+  };
+
+  dynamoDb.put(params, (error) => {
+    if (error) {
+      console.log(error);
+      res.status(400).json({ error: 'Could not create pet' });
+    }
+    res.json({ petId, name });
+  });
+})
+
+module.exports.handler = serverless(app);
